f2846c90e6657eef928d6f051aa97f74366b8615
Ashwinimaregouda2 <ashwini.maregouda@gmail.com>
2025-03-13 09:45:08 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,19 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+ resource "aws_s3_bucket" "my_bucket" {
+  bucket = "ashwiniterraformec2new12"  
+
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.id
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-09e143e99e8fa74f9"
   instance_type = "t2.micro"
