27b4faf0e5f7c3b4a07adb2829d7fd8d63da91e3
Ashwinimaregouda2 <ashwini.maregouda@gmail.com>
2025-03-13 10:03:37 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ashwiniterraformec2new12"
+    bucket         = "ashwiniterraformec2new12789"
     key            = "terraform.tfstate"
     region         = "ap-southeast-2"
     encrypt        = true
