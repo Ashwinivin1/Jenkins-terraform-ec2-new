3314473f5ca942a6d9bc3045b758dcb7fb821df8
Ashwinimaregouda2 <ashwini.maregouda@gmail.com>
2025-03-13 10:02:02 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "ashwiniterraformec2new12"
     key            = "terraform.tfstate"
     region         = "ap-southeast-2"
-    dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
 }
